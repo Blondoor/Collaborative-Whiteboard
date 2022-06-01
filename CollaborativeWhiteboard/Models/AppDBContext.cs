db0bcb812fa72d2063a166a2826bf9bca5e4df95
Dan Bondor <57989629+Blondoor@users.noreply.github.com>
2022-06-01 13:20:20 +0300

    crerated api app and migration for login/rergister

---
+++ CollaborativeWhiteboard/Models/AppDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollaborativeWhiteboard.Models
+{
+    public class AppDBContext: IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options): base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
